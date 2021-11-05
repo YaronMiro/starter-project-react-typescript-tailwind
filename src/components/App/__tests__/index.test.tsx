55f64f32c9cee0a8fcc19d3097da3658e2ebead2
Yaron Miro <yaron.m@webpals.com>
2021-11-05 19:31:02 +0200

    update App component

--- src/components/App/__tests__/index.test.tsx
+++ src/components/App/__tests__/index.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react'
 import App from '@components/App'
 
 test('renders learn react link', () => {
-    render(<App />)
+    render(<App name="Starter Project APP" />)
     const linkElement = screen.getByText(/learn react/i)
     expect(linkElement).toBeInTheDocument()
 })
